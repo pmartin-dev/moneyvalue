b9a32f2475f9471e5827722a0d711cf522454256
Pierre <pm4rtin@gmail.com>
2019-11-06 12:17:26 +0100

    insertion du nom du pays et du drapeau dans les élements pays

--- src/components/rate-exchange-list-item.js
+++ src/components/rate-exchange-list-item.js
@@ -2,9 +2,17 @@ import React, { Component } from "react";
 
 class RateExchangeListItem extends Component {
   render() {
+    console.log(this.props.rateExchange);
+    const { flag, name, rates } = this.props.rateExchange;
+
     return (
       <tr>
-        <td>Pays</td>
+        <td>
+          {name}
+          <br />
+          <img src={flag} height="60" width="100" alt={"flag_" + name} />
+        </td>
+
         <td>Graphique</td>
       </tr>
     );
